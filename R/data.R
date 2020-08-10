69489122cb572d8fb19331a8f0489449e130cec8
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-08-10 14:43:43 +0200

    export got_chars data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Game of Thrones POV characters
+#'
+#' The data is from the [repurrrsive package](https://github.com/jennybc/repurrrsive).
+#'
+#' Info on the point-of-view (POV) characters from the first five books in the
+#' Song of Ice and Fire series by George R. R. Martin. Retrieved from An API Of
+#' Ice And Fire.
+#'
+#' @format A unnamed list with 30 components, each representing a POV character.
+#'   Each character's component is a named list of length 18, containing
+#'   information such as name, aliases, and house allegiances.
+#'
+#' @family Game of Thrones data and functions
+#' @source <https://anapioficeandfire.com>
+#' @examples
+#' got_chars
+#' str(lapply(got_chars, `[`, c("name", "culture")))
+"got_chars"
