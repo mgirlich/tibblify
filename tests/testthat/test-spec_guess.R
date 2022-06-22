a17f2071743869b5e7eac4966799923dd564c266
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-06-22 08:38:32 +0000

    Move `read_sample_json()` to helper file

--- tests/testthat/test-spec_guess.R
+++ tests/testthat/test-spec_guess.R
@@ -16,11 +16,6 @@ test_that("can guess spec for got_chars", {
   expect_snapshot(spec_guess(got_chars) %>% print())
 })
 
-read_sample_json <- function(x) {
-  path <- system.file("jsonexamples", x, package = "tibblify")
-  jsonlite::fromJSON(path, simplifyDataFrame = FALSE)
-}
-
 test_that("can guess spec for citm_catalog", {
   x <- read_sample_json("citm_catalog.json")
   x$areaNames <- x$areaNames[1:3]
@@ -29,6 +24,8 @@ test_that("can guess spec for citm_catalog", {
   x$seatCategoryNames <- x$seatCategoryNames[1:3]
   x$subTopicNames <- x$subTopicNames[1:3]
 
+  # TODO `$seatCategoryNames`, `$subTopicNames`, `$topicNames` can be simplifed to a character vector
+  # TODO think about `$topicSubTopics`
   expect_snapshot(spec_guess(x))
 
   expect_snapshot(spec_guess_list(x, simplify_list = FALSE))
