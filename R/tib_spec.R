c5a5df33b8a78897319ce9475135ee8d5453ac26
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-11-28 13:09:34 +0000

    Remove `purrr::discard()`

--- R/tib_spec.R
+++ R/tib_spec.R
@@ -148,7 +148,6 @@ is_tspec <- function(x) {
 }
 
 prep_spec_fields <- function(fields, call) {
-  fields <- purrr::discard(fields, is_null)
   fields <- flatten_fields(fields)
   if (is_null(fields)) {
     return(list())
