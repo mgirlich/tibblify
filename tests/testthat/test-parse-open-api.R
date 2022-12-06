b75e7018f6e1fb5d79ba80618219fb1b13f4378a
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-12-06 12:27:02 +0000

    Suppress warning

--- tests/testthat/test-parse-open-api.R
+++ tests/testthat/test-parse-open-api.R
@@ -1,5 +1,8 @@
 test_that("can parse open api spec", {
   expect_no_error(
-    parse_openapi_spec("https://dtrnk0o2zy01c.cloudfront.net/openapi/en-us/dest/SponsoredProducts_prod_3p.json")
+    # supprss `incomplete final line` warning
+    suppressWarnings(
+      parse_openapi_spec("https://dtrnk0o2zy01c.cloudfront.net/openapi/en-us/dest/SponsoredProducts_prod_3p.json")
+    )
   )
 })
